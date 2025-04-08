f2f82ef27089f09a8387888113d21df0c468deab
Minor Gordon <minorg@server.fake>
2025-04-08 19:17:58 -0400

    write to /data

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
+import fs from "node:fs";
+import path from "node:path";
 import { createMergeableStore } from "tinybase";
 import { createFilePersister } from "tinybase/persisters/persister-file";
 import { createWsServer } from "tinybase/synchronizers/synchronizer-ws-server";
 import { WebSocketServer } from "ws";
 
+const dataDirectoryPath = fs.existsSync("/data") ? "/data" : ".";
+
 const server = createWsServer(new WebSocketServer({ port: 8080 }), (pathId) =>
   createFilePersister(
     createMergeableStore(),
-    `${pathId.replace(/[^a-zA-Z0-9]/g, "-")}.json`,
+    path.join(
+      dataDirectoryPath,
+      `${pathId.replace(/[^a-zA-Z0-9]/g, "-")}.json`,
+    ),
   ),
 );
