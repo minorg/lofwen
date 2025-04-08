6f770ea8690330b7291960b22bcf787f4399c177
Minor Gordon <minorg@server.fake>
2025-04-08 19:38:51 -0400

    tweaks to synchronization

--- app/app/(authenticated)/_layout.tsx
+++ app/app/(authenticated)/_layout.tsx
@@ -1,7 +1,7 @@
 import "~/global.css";
 import {} from "@react-navigation/native";
 import { Redirect, Stack } from "expo-router";
-import { Fragment, useMemo } from "react";
+import { type PropsWithChildren, useMemo, useState } from "react";
 import { createWsSynchronizer } from "tinybase/synchronizers/synchronizer-ws-client/with-schemas";
 import { Hrefs } from "~/Hrefs";
 import { configuration } from "~/configuration";
@@ -11,39 +11,68 @@ import type { AuthenticatedUser } from "~/models";
 import { SynchronizedStore } from "~/stores/SynchronizedStore";
 
 function Synchronization({
+  children,
   serverUrl,
   store,
   user,
-}: { serverUrl: string; store: SynchronizedStore; user: AuthenticatedUser }) {
+}: PropsWithChildren<{
+  serverUrl: string;
+  store: SynchronizedStore;
+  user: AuthenticatedUser;
+}>) {
   const { useCreateSynchronizer } = SynchronizedStore.UiReact;
-  useCreateSynchronizer(
-    store!,
-    async (store: SynchronizedStore) => {
-      const webSocketUrl = `${serverUrl}${user["@id"]}`;
-      logger.info("synchronizing with", webSocketUrl);
+  const [synchronizerStartedSync, setSynchronizerStartedSync] = useState(false);
+  useCreateSynchronizer(store!, async (store: SynchronizedStore) => {
+    const webSocketUrl = `${serverUrl}${user["@id"]}`;
+    logger.info("synchronizing with", webSocketUrl);
 
-      const synchronizer = await createWsSynchronizer(
-        store,
-        // new ReconnectingWebSocket(webSocketUrl, [], {
-        //   maxReconnectionDelay: 1000,
-        //   connectionTimeout: 1000,
-        // }),
-        new WebSocket(webSocketUrl),
-      );
+    const synchronizer = await createWsSynchronizer(
+      store,
+      // new ReconnectingWebSocket(webSocketUrl, [], {
+      //   maxReconnectionDelay: 1000,
+      //   connectionTimeout: 1000,
+      // }),
+      new WebSocket(webSocketUrl),
+    );
 
-      await synchronizer.startSync();
+    synchronizer.addStatusListener((_, status) => {
+      let statusString: string;
+      switch (status) {
+        case 0:
+          statusString = "idle";
+          break;
+        case 1:
+          statusString = "loading";
+          break;
+        case 2:
+          statusString = "saving";
+          break;
+        default:
+          statusString = "unknown";
+          break;
+      }
+      logger.debug(`synchronization status: ${statusString}`);
+    });
 
-      // If the websocket reconnects in the future, do another explicit sync.
-      synchronizer.getWebSocket().addEventListener("open", () => {
-        synchronizer.load().then(() => synchronizer.save());
-      });
+    await synchronizer.startSync();
 
-      return synchronizer;
-    },
-    [],
-  );
+    logger.info("started sync with", webSocketUrl);
+    setSynchronizerStartedSync(true);
 
-  return <Fragment />;
+    // If the websocket reconnects in the future, do another explicit sync.
+    synchronizer.getWebSocket().addEventListener("open", () => {
+      synchronizer.load().then(() => synchronizer.save());
+    });
+
+    return synchronizer;
+  });
+
+  if (!synchronizerStartedSync) {
+    logger.debug("synchronizer hasn't started sync, not rendering");
+    return null;
+  }
+  logger.debug("synchronized has started sync, rendering");
+  return <>{children}</>;
 }
 
 export default function AuthenticatedLayout() {
@@ -54,6 +83,8 @@ export default function AuthenticatedLayout() {
     return <Redirect href={Hrefs.signIn} />;
   }
 
+  const stack = <Stack screenOptions={{ headerShown: false }} />;
+
   return (
     <SynchronizedStore.UiReact.Provider store={store}>
       {configuration.synchronization !== null ? (
@@ -61,9 +92,12 @@ export default function AuthenticatedLayout() {
           serverUrl={configuration.synchronization.serverUrl!}
           store={store}
           user={user}
-        />
-      ) : null}
-      <Stack screenOptions={{ headerShown: false }} />
+        >
+          {stack}
+        </Synchronization>
+      ) : (
+        stack
+      )}
     </SynchronizedStore.UiReact.Provider>
   );
 }
