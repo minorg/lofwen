6fcb09ccea6b56dbe07c481a35ba293197f96d2a
Minor Gordon <minorg@server.fake>
2025-04-08 18:26:38 -0400

    running server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
+import { createMergeableStore } from "tinybase";
+import { createFilePersister } from "tinybase/persisters/persister-file";
+import { createWsServer } from "tinybase/synchronizers/synchronizer-ws-server";
+import { WebSocketServer } from "ws";
 
+const server = createWsServer(new WebSocketServer({ port: 8080 }), (pathId) =>
+  createFilePersister(
+    createMergeableStore(),
+    `${pathId.replace(/[^a-zA-Z0-9]/g, "-")}.json`,
+  ),
+);
