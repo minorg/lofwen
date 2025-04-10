3853fbe45a4d3d6f427756f807bdc6c10edf4679
Minor Gordon <minorg@server.fake>
2025-04-10 17:35:52 -0400

    add logging to useColorScheme

--- app/hooks/useColorScheme.ts
+++ app/hooks/useColorScheme.ts
@@ -1,8 +1,17 @@
 import { useColorScheme as useNativewindColorScheme } from "nativewind";
+import { useColorScheme as useReactNativeColorScheme } from "react-native";
+import { logger } from "~/logger";
 
 export function useColorScheme() {
   const { colorScheme, setColorScheme, toggleColorScheme } =
     useNativewindColorScheme();
+
+  logger.debug("color scheme according to Nativewind:", colorScheme);
+  logger.debug(
+    "color scheme according to React Native:",
+    useReactNativeColorScheme(),
+  );
+
   return {
     colorScheme: colorScheme ?? "dark",
     isDarkColorScheme: colorScheme === "dark",
