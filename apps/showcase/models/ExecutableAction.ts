dc3949661f2e9c39f90fcbc3bdb9a3988763e47e
Minor Gordon <minorg@server.fake>
2025-04-16 12:26:26 -0400

    documentation of actions

--- apps/showcase/models/ExecutableAction.ts
+++ apps/showcase/models/ExecutableAction.ts
@@ -2,6 +2,11 @@ import type { TinyBaseEventLog } from "@lofwen/event-log";
 import { Action } from "~/models/Action";
 import type { Event } from "~/models/Event";
 
+/**
+ * An action that should be executed in a useEffect.
+ *
+ * Typically adds to the event log, possibly asynchronously.
+ */
 export abstract class ExecutableAction extends Action {
   abstract execute(parameters: {
     eventLog: TinyBaseEventLog<Event>;
--- apps/showcase/models/NopAction.ts
+++ apps/showcase/models/NopAction.ts
@@ -1,5 +1,8 @@
 import { Action } from "~/models/Action";
 
+/**
+ * An action that is neither executable nor renderable but leaves the current page as-is, possibly until some subsequent event occurs.
+ */
 export class NopAction extends Action {
   override toString(): string {
     return "NopAction";
