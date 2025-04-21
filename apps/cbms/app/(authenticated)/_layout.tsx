295bb4d7a36fa2db377413a18f70be4a48402f21
Minor Gordon <minorg@server.fake>
2025-04-21 18:58:48 -0400

    comment out persister again

--- apps/cbms/app/(authenticated)/_layout.tsx
+++ apps/cbms/app/(authenticated)/_layout.tsx
@@ -2,20 +2,25 @@ import "~/global.css";
 import { TinyBaseEventLog } from "@lofwen/event-log";
 import {} from "@react-navigation/native";
 import { Slot } from "expo-router";
+import { rootLogger } from "~/rootLogger";
 
-// const logger = rootLogger.extend("Persister");
+const logger = rootLogger.extend("Persister");
 
 export default function AuthenticatedLayout() {
   const { useCreateStore } = TinyBaseEventLog.UiReact;
-  const store = useCreateStore(() => TinyBaseEventLog.Store.create());
-  // useCreatePersister(
+  const store = useCreateStore(() => {
+    const store = TinyBaseEventLog.Store.create();
+    logger.debug("created store");
+    return store;
+  });
+  // const persister = useCreatePersister(
   //   store,
   //   (store) => {
   //     if (Platform.OS === "web") {
   //       const {
   //         createSessionPersister,
   //       } = require("tinybase/persisters/persister-browser/with-schemas");
-  //       const persister = createSessionPersister(store, "cbms-app");
+  //       const persister = createSessionPersister(store, "cbms-app2");
   //       logger.debug("created browser session persister");
   //       return persister;
   //     }
@@ -31,12 +36,20 @@ export default function AuthenticatedLayout() {
   //   },
   //   undefined,
   //   async (persister) => {
+  //     await persister.load();
+  //     logger.debug("loaded from persister");
   //     await persister.startAutoLoad();
   //     logger.debug("started persister auto-load");
   //     await persister.startAutoSave();
   //     logger.debug("started persister auto-save");
   //   },
+  //   undefined,
+  //   () => {
+  //     logger.debug("destroyed persister");
+  //   },
   // );
+  // const persisterStatus = usePersisterStatus(persister);
+  // logger.debug(`persister status: ${persisterStatus}`);
 
   return (
     <TinyBaseEventLog.UiReact.Provider store={store}>
