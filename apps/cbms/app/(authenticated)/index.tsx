977a3e4b565d78b61cc6b6b74e7dd6f408dd8e44
Minor Gordon <minorg@server.fake>
2025-04-21 16:20:34 -0400

    overwrite existing actions/events

--- apps/cbms/app/(authenticated)/index.tsx
+++ apps/cbms/app/(authenticated)/index.tsx
@@ -2,7 +2,7 @@ import { Timestamp } from "@lofwen/models";
 import { useEffect, useMemo } from "react";
 import invariant from "ts-invariant";
 import { useEventLog } from "~/hooks/useEventLog";
-import { NopAction } from "~/models/NopAction";
+import { ExecutableAction } from "~/models/ExecutableAction";
 import { RenderableAction } from "~/models/RenderableAction";
 import { rootLogger } from "~/rootLogger";
 import { workflow } from "~/workflow";
@@ -12,15 +12,20 @@ const logger = rootLogger.extend("RootScreen");
 export default function RootScreen() {
   logger.debug("rendering");
   const eventLog = useEventLog();
-  const action = useMemo(() => workflow({ eventLog }), [eventLog]);
-  logger.debug(`action: ${action}`);
+  const nextAction = useMemo(() => workflow({ eventLog }), [eventLog]);
+  logger.debug(`next action: ${nextAction}`);
   // biome-ignore lint/correctness/useExhaustiveDependencies: run useEffect once
   useEffect(() => {
     eventLog.append({ "@type": "StartedAppEvent", timestamp: Timestamp.now() });
   }, []);
-  if (action instanceof NopAction) {
-    return null;
+  useEffect(() => {
+    if (nextAction instanceof ExecutableAction) {
+      nextAction.execute({ eventLog });
+    }
+  }, [eventLog, nextAction]);
+  if (nextAction instanceof RenderableAction) {
+    return nextAction.render();
   }
-  invariant(action instanceof RenderableAction);
-  return (action as RenderableAction).render();
+  invariant(nextAction instanceof ExecutableAction);
+  return null;
 }
--- apps/cbms/models/NopAction.ts
+++ apps/cbms/models/NopAction.ts
@@ -1,14 +1,16 @@
-import { Action } from "~/models/Action";
+import { ExecutableAction } from "~/models/ExecutableAction";
 
 /**
  * An action that is neither executable nor renderable but leaves the current page as-is, possibly until some subsequent event occurs.
  */
-export class NopAction extends Action {
+export class NopAction extends ExecutableAction {
   static readonly instance = new NopAction();
 
   private constructor() {
     super();
   }
+
+  override async execute(): Promise<void> {}
 
   override toString(): string {
     return "NopAction";
