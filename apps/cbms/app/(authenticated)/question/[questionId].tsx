616b42c7c45935f0abc01bcaee47874acfab2bfa
Minor Gordon <minorg@server.fake>
2025-04-21 16:21:21 -0400

    overwrite question screen

--- apps/cbms/app/(authenticated)/question/[questionId].tsx
+++ apps/cbms/app/(authenticated)/question/[questionId].tsx
@@ -4,19 +4,18 @@ import {
   type TextAnswer,
   Timestamp,
 } from "@lofwen/models";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
 import { type ReactElement, useCallback, useEffect, useMemo } from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import invariant from "ts-invariant";
+import { Hrefs } from "~/Hrefs";
 import { DichotomousQuestionView } from "~/components/DichotomousQuestionView";
 import { LikertScaleQuestionView } from "~/components/LikertScaleQuestionView";
 import { TextQuestionView } from "~/components/TextQuestionView";
 import { useEventLog } from "~/hooks/useEventLog";
 import type { Answer } from "~/models/Answer";
 import { ExecutableAction } from "~/models/ExecutableAction";
-import { NopAction } from "~/models/NopAction";
-import { PoseQuestionAction } from "~/models/PoseQuestionAction";
 import { RenderableAction } from "~/models/RenderableAction";
 import { rootLogger } from "~/rootLogger";
 import { workflow } from "~/workflow";
@@ -48,11 +47,12 @@ export default function QuestionScreen() {
   const navigation = useNavigation();
 
   const nextAction = useMemo(() => workflow({ eventLog }), [eventLog]);
+  logger.debug(`next action: ${nextAction}`);
 
   const question = useMemo(() => {
     for (const event of eventLog.reverse()) {
       if (
-        event["@type"] === "PosedQuestionEvent" &&
+        event["@type"] === "FormulatedQuestionEvent" &&
         event.question["@id"] === questionId
       ) {
         logger.debug(`have question ${questionId} in event log`);
@@ -82,33 +82,16 @@ export default function QuestionScreen() {
     }
   }, [navigation, question]);
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Run useEffect once
   useEffect(() => {
-    const lastEvent = eventLog.last;
-    // The workflow should return the PoseQuestionAction until the redirect here succeeds and the PosedQuestionEvent is added to the event log
-    if (
-      nextAction instanceof PoseQuestionAction &&
-      nextAction.question["@id"] === questionId
-    ) {
-      if (
-        lastEvent === null ||
-        lastEvent["@type"] !== "PosedQuestionEvent" ||
-        lastEvent.question["@id"] !== questionId
-      ) {
-        logger.debug(
-          `last event is not a PosedQuestionEvent to the current question (${questionId}), appending a PosedQuestionEvent to the event log`,
-        );
-        eventLog.append({
-          "@type": "PosedQuestionEvent",
-          question: nextAction.question,
-          timestamp: Timestamp.now(),
-        });
-      } else {
-        logger.debug(
-          `last event is already a PosedQuestionEvent to the current question (${questionId})`,
-        );
-      }
+    if (question !== null) {
+      eventLog.append({
+        "@type": "PosedQuestionEvent",
+        questionId: question["@id"],
+        timestamp: Timestamp.now(),
+      });
     }
-  }, [eventLog, nextAction, questionId]);
+  }, []);
 
   useEffect(() => {
     if (nextAction instanceof ExecutableAction) {
@@ -116,23 +99,14 @@ export default function QuestionScreen() {
     }
   }, [eventLog, nextAction]);
 
-  if (nextAction instanceof NopAction) {
-    logger.debug("next action is nop");
-  } else if (nextAction instanceof PoseQuestionAction) {
-    if (nextAction.question["@id"] === questionId) {
-      logger.debug("next action would execute to the current page, nop");
-    } else {
-      return nextAction.render();
-    }
-  } else if (nextAction instanceof RenderableAction) {
+  if (nextAction instanceof RenderableAction) {
     return nextAction.render();
-  } else {
-    invariant(nextAction instanceof ExecutableAction);
   }
+  invariant(nextAction instanceof ExecutableAction);
 
   if (question === null) {
-    logger.debug("question not set yet");
-    return null;
+    logger.warn(`no such question: ${questionId}`);
+    return <Redirect href={Hrefs.root} />;
   }
 
   let questionView: ReactElement;
