6061bf5c5a5f6e0dcb235bbe85e573a0a867f68d
Minor Gordon <minorg@server.fake>
2025-04-22 11:58:49 -0400

    disable persistence temporarily

--- apps/cbms/app/(authenticated)/_layout.tsx
+++ apps/cbms/app/(authenticated)/_layout.tsx
@@ -2,70 +2,70 @@ import "~/global.css";
 import { TinyBaseEventLog } from "@lofwen/event-log";
 import {} from "@react-navigation/native";
 import { Slot } from "expo-router";
-import { useEffect, useState } from "react";
-import { Platform } from "react-native";
+import {} from "react";
 import { rootLogger } from "~/rootLogger";
 
 const logger = rootLogger.extend("Persister");
 
-const storageName = "cbms-app-1745336397";
+// const storageName = "cbms-app-1745336397";
 // process.env.NODE_ENV === "development" ? randomUUID() : "cbms-app";
 
 export default function AuthenticatedLayout() {
-  const { useCreatePersister, useCreateStore, usePersisterStatus } =
-    TinyBaseEventLog.UiReact;
-  useEffect(() => {
-    logger.debug("storage name:", storageName);
-  }, []);
+  const { useCreateStore } = TinyBaseEventLog.UiReact;
   const store = useCreateStore(() => {
     const store = TinyBaseEventLog.Store.create();
     logger.debug("created store");
     return store;
   });
-  const [storeLoaded, setStoreLoaded] = useState(false);
-  const persister = useCreatePersister(
-    store,
-    (store) => {
-      if (Platform.OS === "web") {
-        const {
-          createSessionPersister,
-        } = require("tinybase/persisters/persister-browser/with-schemas");
-        const persister = createSessionPersister(store, storageName);
-        logger.debug("created browser session persister");
-        return persister;
-      }
 
-      const { openDatabaseSync } = require("expo-sqlite");
-      const db = openDatabaseSync(storageName);
-      const {
-        createExpoSqlitePersister,
-      } = require("tinybase/persisters/persister-expo-sqlite/with-schemas");
-      const persister = createExpoSqlitePersister(store, db);
-      logger.debug("created Expo SQLite persister");
-      return persister;
-    },
-    undefined,
-    async (persister) => {
-      await persister.load();
-      logger.debug("loaded from persister");
-      setStoreLoaded(true);
-      await persister.startAutoLoad();
-      logger.debug("started persister auto-load");
-      await persister.startAutoSave();
-      logger.debug("started persister auto-save");
-    },
-    undefined,
-    () => {
-      logger.debug("destroyed persister");
-    },
-  );
-  const persisterStatus = usePersisterStatus(persister);
-  logger.debug(`persister status: ${persisterStatus}`);
+  // const { useCreatePersister, usePersisterStatus } = TinyBaseEventLog.UiReact;
+  // useEffect(() => {
+  //   logger.debug("storage name:", storageName);
+  // }, []);
+  // const [storeLoaded, setStoreLoaded] = useState(false);
+  // const persister = useCreatePersister(
+  //   store,
+  //   (store) => {
+  //     if (Platform.OS === "web") {
+  //       const {
+  //         createSessionPersister,
+  //       } = require("tinybase/persisters/persister-browser/with-schemas");
+  //       const persister = createSessionPersister(store, storageName);
+  //       logger.debug("created browser session persister");
+  //       return persister;
+  //     }
 
-  if (!storeLoaded) {
-    logger.debug("store is not loaded yet, not rendering");
-    return null;
-  }
+  //     const { openDatabaseSync } = require("expo-sqlite");
+  //     const db = openDatabaseSync(storageName);
+  //     const {
+  //       createExpoSqlitePersister,
+  //     } = require("tinybase/persisters/persister-expo-sqlite/with-schemas");
+  //     const persister = createExpoSqlitePersister(store, db);
+  //     logger.debug("created Expo SQLite persister");
+  //     return persister;
+  //   },
+  //   undefined,
+  //   async (persister) => {
+  //     await persister.load();
+  //     logger.debug("loaded from persister");
+  //     setStoreLoaded(true);
+  //     await persister.startAutoLoad();
+  //     logger.debug("started persister auto-load");
+  //     await persister.startAutoSave();
+  //     logger.debug("started persister auto-save");
+  //   },
+  //   undefined,
+  //   () => {
+  //     logger.debug("destroyed persister");
+  //   },
+  // );
+  // const persisterStatus = usePersisterStatus(persister);
+  // logger.debug(`persister status: ${persisterStatus}`);
+
+  // if (!storeLoaded) {
+  //   logger.debug("store is not loaded yet, not rendering");
+  //   return null;
+  // }
 
   return (
     <TinyBaseEventLog.UiReact.Provider store={store}>
