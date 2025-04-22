2b298d1a17ea0870309a82defa0ff5cce6654e3b
Minor Gordon <minorg@server.fake>
2025-04-21 20:08:20 -0400

    more logging

--- apps/cbms/app/(authenticated)/instructions/_layout.tsx
+++ apps/cbms/app/(authenticated)/instructions/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 import { screenOptions } from "~/components/screenOptions";
 
-export default function QuestionLayout() {
+export default function InstructionsLayout() {
   return <Stack screenOptions={{ ...screenOptions }} />;
 }
