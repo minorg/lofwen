a137eef7c5b3fa94f433c9736eb0ad26f4b36a81
Minor Gordon <minorg@server.fake>
2025-04-24 14:34:48 -0400

    hacking colors

--- apps/cbms/app/(authenticated)/(tabs)/_layout.tsx
+++ apps/cbms/app/(authenticated)/(tabs)/_layout.tsx
@@ -2,11 +2,22 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { Tabs } from "expo-router";
 import { DefaultReactNavigationThemeProvider } from "~/components/ReactNavigationThemeProvider";
 import { screenOptions } from "~/components/screenOptions";
+import { useColorScheme } from "~/hooks/useColorScheme";
 
 export default function TabLayout() {
+  const { isDarkColorScheme } = useColorScheme();
   return (
     <DefaultReactNavigationThemeProvider>
-      <Tabs screenOptions={{ ...screenOptions }}>
+      <Tabs
+        screenOptions={{
+          ...screenOptions,
+          // Hack to avoid white overlay
+          tabBarActiveBackgroundColor: isDarkColorScheme ? "black" : "white",
+          tabBarActiveTintColor: isDarkColorScheme ? "white" : "black",
+          tabBarInactiveBackgroundColor: isDarkColorScheme ? "black" : "white",
+          tabBarInactiveTintColor: isDarkColorScheme ? "white" : "black",
+        }}
+      >
         <Tabs.Screen
           name="garden"
           options={{
--- apps/cbms/components/GardenItemView.tsx
+++ apps/cbms/components/GardenItemView.tsx
@@ -2,24 +2,17 @@ import { Pressable, View } from "react-native";
 import { IconView } from "~/components/IconView";
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
-import { useColorScheme } from "~/hooks/useColorScheme";
 import type { GardenItem } from "~/models/GardenItem";
 
 export function GardenItemView({
   gardenItem,
   onSelect,
 }: { gardenItem: GardenItem; onSelect: () => void }) {
-  const { isDarkColorScheme } = useColorScheme();
-
   return (
     <View className="border-2 border-primary flex flex-row gap-4 p-4 rounded-sm">
       <View className="flex flex-col justify-center">
         <Pressable onPress={onSelect}>
-          <IconView
-            color={isDarkColorScheme ? "white" : undefined}
-            icon={gardenItem.icon}
-            size={64}
-          />
+          <IconView icon={gardenItem.icon} size={64} />
         </Pressable>
       </View>
       <View className="flex flex-col flex-1 items-end">
