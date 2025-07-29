928969ae95a74255b2173b3a3e83b937b63d419b
Minor Gordon <minorg@server.fake>
2025-07-29 19:11:15 -0400

    integrate images

--- apps/cbms/components/InstructionsView.tsx
+++ apps/cbms/components/InstructionsView.tsx
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { Image, View } from "react-native";
+import images from "~/assets/images";
 import { Markdown } from "~/components/Markdown";
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
@@ -12,8 +13,19 @@ export function InstructionsView({
   onAcknowledge: () => void;
 }) {
   return (
-    <View className="flex flex-col flex-1 gap-2 native:p-4">
-      <Markdown>{instructions.text}</Markdown>
+    <View className="flex flex-col flex-1">
+      {instructions.image ? (
+        <Image
+          source={images[instructions.image]}
+          style={{
+            flex: 1,
+            width: "100%",
+            height: "100%",
+            resizeMode: "contain",
+          }}
+        />
+      ) : null}
+      {instructions.text ? <Markdown>{instructions.text}</Markdown> : null}
       <Button onPress={onAcknowledge} variant="outline">
         <Text>OK</Text>
       </Button>
