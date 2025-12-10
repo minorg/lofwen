3e53275e61b123a68ccbd2c703113beb14846fdf
Minor Gordon <minorg@server.fake>
2025-12-09 19:20:04 -0500

    fix metro configs

--- apps/cbms/metro.config.js
+++ apps/cbms/metro.config.js
@@ -1,20 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require("node:path");
 
-// Find the project and workspace directories
-const projectRoot = __dirname;
-// This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, "../..");
+// As of Expo SDK 52, no longer need to manually configure Metro for monorepos: https://docs.expo.dev/guides/monorepos/
 
-const config = getDefaultConfig(projectRoot);
-
-// 1. Watch all files within the monorepo
-config.watchFolders = [monorepoRoot];
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(monorepoRoot, "node_modules"),
-];
+const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: "./global.css" });
